8ab8f9030e55ce54e7a8657b5a824ce470d47f52
Tanishka Sharma <25110cn532@glbitm.ac.in>
2026-06-09 16:10:11 +0530

    Add files via upload

---
+++ day9.c/question33.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    if(n <= 0)
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+
+    for(int i = n; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ day9.c/question35.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    if(n <= 0 || n > 26)
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%c", 'A' + i - 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
