7f10d76613e59aed886767c222fac78e0ce1ee1f
Tanishka Sharma <25110cn532@glbitm.ac.in>
2026-06-16 20:21:23 +0530

    Add files via upload

---
+++ day16.c/question61.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    int arr[n - 1];
+
+    printf("Enter %d elements:\n", n - 1);
+
+    int sum = 0;
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    int total = n * (n + 1) / 2;
+
+    printf("Missing Number = %d", total - sum);
+
+    return 0;
+}
