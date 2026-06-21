5118eb7a733bbdbab63cb856239395ce6e1cacc2
Tanishka Sharma <25110cn532@glbitm.ac.in>
2026-06-21 22:05:58 +0530

    Add files via upload

---
+++ day21.c/question83.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int vowels = 0, consonants = 0;
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while(str[i] != '\0')
+    {
+        char ch = str[i];
+
+        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                vowels++;
+            else
+                consonants++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
